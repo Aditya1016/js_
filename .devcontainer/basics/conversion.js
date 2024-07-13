380146f6db8ec8fc1583c195fc4f92d50e182eb3
Aditya Sinha <iamaditya.sinha16@gmail.com>
2024-07-13 11:53:41 +0000

    .

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,28 @@
+// let score = undefined
+
+// console.log(typeof score);
+// // console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// // console.log(typeof(valueInNumber));
+
+// console.log(score);
+// console.log(valueInNumber);
+
+// ********************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+console.log(+true);
+console.log(+"");
