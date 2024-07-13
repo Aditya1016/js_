38dcda9b420e73f2469b53053775acb7efc098c0
Aditya Sinha <iamaditya.sinha16@gmail.com>
2024-07-13 07:54:48 +0000

    .

---
+++ .devcontainer/basics/var.js
@@ -0,0 +1,15 @@
+const accountId = 13345
+let accountName = "hitesh"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table([accountId, accountName, accountPassword, accountCity])
+
+// accountId = 2
+accountName = "Aditya"
+accountPassword = "54321"
+accountCity = "Bengaluru"
+
+console.log(accountPassword);
+
+console.table([accountId, accountName, accountPassword, accountCity])
