34c89d96734d2eb24459da30aadbb439a8242dad
Aditya Sinha <iamaditya.sinha16@gmail.com>
2024-07-13 14:12:09 +0000

    .

---
+++ .devcontainer/basics/dataTypesSummary.js
@@ -0,0 +1,19 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 12345678901234456n
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Aditya",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+} 
+
+console.log(typeof bigNumber);
