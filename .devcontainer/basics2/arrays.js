71ee7119de0e57abd3a6e299580cfc49c151781a
Aditya Sinha <iamaditya.sinha16@gmail.com>
2024-07-15 14:38:22 +0000

    Till objects

---
+++ .devcontainer/basics2/arrays.js
@@ -0,0 +1,32 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+// console.log(myArray[1]);
+
+// methods
+
+// myArray.push(6);
+// myArray.pop();
+
+// myArray.unshift(9);
+// myArray.shift();
+
+// console.log(myArray.includes(1));
+
+// console.log(myArray);
+
+// const newArr = myArray.join();
+
+// console.log(newArr);
+
+console.log("A", myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(1,3)
+
+console.log(myn2);
+console.log("C", myArray);
